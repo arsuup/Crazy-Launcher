8918f58f69f784f5dfb1f9a726047b8545a1ca5a
arsuup <90505488+arsuup@users.noreply.github.com>
2026-01-26 09:31:59 +0100

    update from server

--- instances/php/instances.php
+++ instances/php/instances.php
@@ -1,5 +1,5 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['test'] = array_merge($instance['test'], array(
     "loader" => array(
         "minecraft_version" => "1.8.9",
         "loader_type" => "forge",
@@ -24,23 +24,6 @@ $instance['hypixel'] = array_merge($instance['hypixel'], array(
         "nameServer" => "Hypixel",
         "ip" => "mc.hypixel.net",
         "port" => 25565
-    )
-));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loader" => array(
-        "minecraft_version" => "1.16.5",
-        "loader_type" => "forge",
-        "loader_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
     )
 ));
 ?>
